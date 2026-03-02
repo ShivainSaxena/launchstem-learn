83508c5f8212a2a9fc1165ec394fac8a09de6be7
Shivain Saxena <shivainsaxena@gmail.com>
2026-03-02 11:56:33 -0500

    Updated target api IP

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://34.121.90.25:4000",
+        target: "http://34.71.28.255:4000",
         // target: "http://localhost:4000",
         changeOrigin: true,
         secure: false,
